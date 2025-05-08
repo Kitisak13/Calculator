eddee15b0db16dd41020a487826b12448c1289e1
Kitisak Junsongsang <kitisak.junsong@gmail.com>
2025-05-08 16:53:41 +0700

    add cal

---
+++ calculator.py
@@ -0,0 +1,5 @@
+from tkinter import *
+from tkinter import messagebox
+from math import pi, sqrt, sin, cos, tan, log10, log2, log, exp, factorial, degrees, radians, asin, acos, atan, sinh, cosh, tanh
+from tkinter import ttk
+
